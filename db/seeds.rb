0ba8ab4acdff1d5a3c435f6d2be2e6b35f96f7df
KaraJH <karas.jones@yahoo.com>
2023-01-25 11:46:14 -0700

    Artist songs controller and tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+@prince = Artist.create!(name: "Prince")
+@purple = @prince.songs.create!(title: "Purple Rain", length: 12, play_count: 34)
+@beret = @prince.songs.create!(title: "Raspberry Beret", length: 98, play_count: 94)
