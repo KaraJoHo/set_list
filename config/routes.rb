e0a240bb9fd31bca193576e365fc0bd80b971192
KaraJH <karas.jones@yahoo.com>
2023-01-26 11:17:18 -0700

    Adds artist new and create

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   get '/songs', to:'songs#index'
   get '/songs/:id', to: 'songs#show'
-  get '/artists/:artist_id/songs', to: 'artist_songs#index'
+  get '/artists/:artist_id/songs', to: 'artist_songs#index' #how could we do this before having an artists controller?
+  get '/artists', to: 'artists#index'
+  get '/artists/new', to: 'artists#new'
+
+  post '/artists', to: 'artists#create'
   
 end
